96625ce0627ef5121a9507ef1130079ee54019eb
onubrooks <abahonuh@gmail.com>
2024-04-11 15:28:03 +0100

    set up terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,95 @@
+variable "credentials" {
+  description = "Path to the keyfile containing GCP credentials."
+  type        = string
+  default     = "../mage/retail_sales_etl/secrets/google.json"
+}
+
+
+variable "project" {
+  description = "Project"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default = "EU"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default = "zoomcamp_de_411412_demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default = "zoomcamp-de-411412-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Retail Sales Analytics"
+  default     = "retail-sales-analytics"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "europe-west1"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "europe-west1-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-prep"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The Docker image url in the Artifact Registry repository to be deployed to Cloud Run"
+  default     = "region-docker.pkg.dev/project_id/repository_name/mageai"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
